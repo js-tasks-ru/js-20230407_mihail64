45e6d14e8ce5cd9841025fdbadea04b1d0715a6d
Chekh Mikhail <m.chekh@qiwi.com>
2023-04-18 20:37:00 +0300

    fix sort

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,15 +4,13 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-  let sortedArr = [...arr];
-  sortedArr.sort(compareStrings);
-  if (param === "desc") {
-    sortedArr.reverse();
+export function sortStrings(arr, param = "asc") {
+  const order = {
+    "asc": (a, b) => a.localeCompare(b, ["ru", "en"], { sensitivity: "case", caseFirst: "upper" }),
+    "desc": (a, b) => b.localeCompare(a, ["ru", "en"], { sensitivity: "case", caseFirst: "upper" })
+  };
+  if (order[param] === undefined) {
+    throw new Error(`unknown sorting type ${param}`);
   }
-  return sortedArr;
-}
-
-function compareStrings(a, b) {
-  return a.localeCompare(b, "ru", { sensitivity: "case", caseFirst: "upper" });
+  return [...arr].sort(order[param]);
 }
