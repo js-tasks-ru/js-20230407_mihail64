b79649b7fa7cfba47b2aac204c684b66415d9085
Chekh Mikhail <m.chekh@qiwi.com>
2023-04-21 12:58:12 +0300

    3

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,14 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  return function (obj) {
+    return recursiveGetter(obj, path.split('.'));
+  } ;
+}
 
+function recursiveGetter(obj, pathArr) {
+  if (pathArr.length === 0 || obj === undefined) {
+    return obj;
+  }
+  return recursiveGetter(obj[pathArr[0]], pathArr.slice(1));
 }
